ce75b54f1fa3fbea795b3993a6e26b78e1517b0b
wiplal <vaclav.sima@student.sps-cl.cz>
2024-01-09 16:51:02 +0100

    commit

---
+++ HTML-UKOL3/index.js
@@ -0,0 +1,8 @@
+function vytvoritTabulku(){
+    console.log(document.getElementById("pocet-radku").value);
+    console.log(document.getElementById("pocet-sloupcu").value)
+
+let Tabulka = document.createElement("pocet-radku")
+
+}
+
