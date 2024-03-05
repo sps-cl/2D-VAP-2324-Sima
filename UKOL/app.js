794a758fd79c25435950972e6fa1793790410365
wiplal <vaclav.sima@student.sps-cl.cz>
2024-03-05 16:03:56 +0100

    vytvoreni

---
+++ UKOL/app.js
@@ -0,0 +1,8 @@
+import Person from "./person";
+const express = require("person");
+
+app.use(express.static(__dirname,))
+
+app.get("person")
+
+app.listen(7707);
---
+++ UKOL/person.js
@@ -0,0 +1,9 @@
+export class Person{
+    constructor(name,surname,id){
+
+    this.name = randomly(name);
+    this.surname = random(surname);
+    this.id = random(id);
+    }
+
+}
