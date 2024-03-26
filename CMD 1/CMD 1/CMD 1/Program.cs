19ba0077cce6bbcfbd56c20837a027d5b4b29e02
wiplal <vaclav.sima@student.sps-cl.cz>
2024-03-26 16:35:49 +0100

    vytvoreni

---
+++ CMD 1/CMD 1/CMD 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMD_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte příklad");
+            Console.ReadLine();
+            double priklad = double.Parse(Console.ReadLine());
+            char num = num;
+
+
+
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,8 @@
+Dobrý den
+
+Dodělávám tento úkol z jiné učebny a po zbytek doby odevzdání se nedostanu do ict5 a git mě nechce pustit udělat push do remote repozitáře a přihlášení tokenem z nějakého důvodu nefunguje.
+A tak se velmi omlouvám ale nezbývá mi nic jiného než vám to poslat jako zazipovanou složku.
+
+Doufám že mi to prominete, už se to nestane.
+
+Děkuji, Vácalv Šíma
