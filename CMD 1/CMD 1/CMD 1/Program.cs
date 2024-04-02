43502cb20545028b6f8a4e3d3df542c5c321b02b
wiplal <vaclav.sima@student.sps-cl.cz>
2024-04-02 16:52:35 +0200

    vytvoreni

--- CMD 1/CMD 1/CMD 1/Program.cs
+++ CMD 1/CMD 1/CMD 1/Program.cs
@@ -1,22 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CMD_1
+ 
+namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Zadejte příklad");
-            Console.ReadLine();
-            double priklad = double.Parse(Console.ReadLine());
-            char num = num;
+            while (true)
+            {
+                Console.WriteLine("Zadej sekvenci čísel:");
+                string input = Console.ReadLine();
+                string[] numbers = input.Split(',');
+ 
+                int[] parsedNumbers;
+                try
+                {
+                    parsedNumbers = numbers.Select(int.Parse).ToArray();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Chyba. Zadejte prosím znovu.");
+                    continue;
+                }
+ 
+                int min = parsedNumbers.Min();
+                int max = parsedNumbers.Max();
+                int longestInterval = GetLongestInterval(parsedNumbers);
+
+                Console.WriteLine($"Nejmenší číslo je: {min}.");
+                Console.WriteLine($"Největší číslo je: {max}.");
+                Console.WriteLine($"Délka nejdelšího vzestupného intervalu je: {longestInterval}.");
+                Console.WriteLine("Chcete pokračovat? (ano/ne)");
 
 
-
+                string choice = Console.ReadLine();
+                if (choice.ToLower() != "ano")
+                    break;
+            }
+        }
+ 
+        static int GetLongestInterval(int[] numbers)
+        {
+            int longestInterval = 0;
+            int currentInterval = 1;
+ 
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > numbers[i - 1])
+                    currentInterval++;
+                else
+                {
+                    if (currentInterval > longestInterval);
+                        longestInterval = currentInterval;
+                    currentInterval = 1;
+                }
+            }
+ 
+            if (currentInterval > longestInterval)
+            longestInterval = currentInterval;
+ 
+            return longestInterval;
         }
     }
 }
