93cea35fd686ba8826960bacdacdf76221de2bcb
wiplal <vaclav.sima@student.sps-cl.cz>
2024-04-16 16:49:55 +0200

    vytvoreni

---
+++ CSS2/CSS2/CSS2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+        class ComplexNumber
+        {
+            public double Real;
+            public double Imaginary;
+
+            public Vector(double real, double imaginary)
+            {
+                Real = real;
+                Imaginary = imaginary;
+            }
+            public void Add(double real, double imaginary)
+            {
+                Real = other.Real;
+                Imaginary = other.Imaginary;
+            }
+        }
+}
+}
