50b46a52a25d0b45d149a41d944b1769a01f577c
wiplal <vaclav.sima@student.sps-cl.cz>
2024-04-23 16:44:25 +0200

    vytvoreni

---
+++ CMD3/CMD3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ConsoleApp
+{
+    private List<string> words = new List<string>();
+    private int currentIndex = -1;
+
+    void AddWord(string word)
+    {
+    words.Add(word);
+    currentIndex = words.Count - 1;
+    Console.WriteLine(word);
+    }
+
+    void GoBack()
+    {
+    if (currentIndex > 0)
+    {
+        currentIndex--;
+    }
+    Console.WriteLine(words[currentIndex]);
+    }
+
+    void GoForward()
+    {
+        if (currentIndex < words.Count - 1)
+        {
+            currentIndex++;
+        }
+        Console.WriteLine(words[currentIndex]);
+    }
+
+    void Run()
+    {
+        while (true)
+    {
+
+    //Konzole vypíše vstup
+     Console.Write("Pridat: ");
+     string command = Console.ReadLine();
+
+     if (command.StartsWith("A"))
+            {
+                string word = command.Substring(7);
+                AddWord(word);
+            }
+            else if (command == "Zpet")
+            {
+                GoBack();
+            }
+            else if (command == "Vpred")
+            {
+                GoForward();
+            }
+            else
+            {
+                Console.WriteLine("Neznámý příkaz. Zadejte znovu");
+            }
+        }
+    }
+
+    static void Main()
+    {
+        ConsoleApp app = new ConsoleApp();
+        app.Run();
+    }
+}
